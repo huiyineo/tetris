b745edb662cce3e2a12c7999ad9af60c42c76505
huiyineo <huiyi2109@gmail.com>
2020-10-07 02:18:23 +0800

    add board component

--- src/Tetris/Tetris.js
+++ src/Tetris/Tetris.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Tetris.css";
-
 import NextBlock from "./NextBlock";
 import RotateBlock from "./RotateBlock";
+import Board from "./Board";
+
 
 class Tetris extends React.Component {
   render() {
     return (
       <>
         <div className="game-title">Welcome to Tetris.</div>
+        <Board />
         <NextBlock />
         <RotateBlock />
       </>
