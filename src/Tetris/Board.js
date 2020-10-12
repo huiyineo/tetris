18b50e03f5d6f0ae7ccf0ce06cb7769c315045cd
huiyineo <huiyi2109@gmail.com>
2020-10-12 20:10:14 +0800

    reverse board names

--- src/Tetris/Board.js
+++ src/Tetris/Board.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Dot from "./Commons/Dot";
+import Block from "./Commons/Block";
+import utils from "../Utils/utils";
 
 class Board extends React.Component {
   constructor(props) {
@@ -8,93 +10,172 @@ class Board extends React.Component {
     var dots = new Array(20);
     for (var i = 0; i < dots.length; i++) {
       dots[i] = new Array(10).fill(0);
+      // if(i === 19){
+      //   dots[i] = [2,2,2,2,0,0,2,2,2,2]
+      // }
     }
 
     this.state = {
-      dots: dots,
-      movingBlock: null,
-      gameOver: false      
+      board: dots,
+      block: this.generateMatrixWithBlock(Block.simpleRandom()),
+      position: [-3, 4],
+      blockNo: 1,
+      intervalId: null
     };
   }
 
   componentDidMount() {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       this.moveBlock();
     }, 500);
+    this.setState({intervalId: intervalId});
   }
 
-  moveBlock() {
-    if (this.state.gameOver) {
-      return;
-    }
+  generateMatrixWithBlock(block) {
+    const len = Math.max(block.content.length, block.content[0].length);
 
-    if (this.state.movingBlock == null) {
-      this.setState({
-        movingBlock: this.getNewBlock(),
-      });
-      return;
-    }
+    block.content = utils.blockToMatrix(block.content, len, len);
 
-    let movingBlock = this.state.movingBlock.map((x) => {
-      x += 10;
-      return x;
+    return block;
+  }
+
+  createNewBlock() {
+    this.setState({
+      position: [-3, 4],
+      block: this.generateMatrixWithBlock(Block.tgm3RandomNew()),
     });
+  }
 
-    let dots = this.state.dots;
-    let gameOver = this.state.gameOver;
-    if (this.shouldStop(movingBlock)) {
-      this.state.movingBlock.forEach(
-        (x) => (dots[Math.floor(x / 10)][x % 10] = 1)
-      );
-      const newBlock = this.getNewBlock();
-
-      if (this.shouldStop(newBlock)) {
-        gameOver = true;
-      } else {
-        movingBlock = newBlock;
+  isGameOver() {
+    const board = this.state.board;
+    const len = board[0].length;
+    for (let i = 0; i < len; i++) {
+      if (board[0][i] > 1) {
+        return true;
       }
     }
 
-    this.setState({
-      dots: dots,
-      movingBlock: movingBlock,
-      gameOver: gameOver,
-    });
+    return false;
   }
 
-  shouldStop(movingBlock) {
-    return (
-      movingBlock.some((x) => x >= 200) ||
-      movingBlock.some((x) => this.state.dots[Math.floor(x / 10)][x % 10] === 1)
-    );
+  pinCurrentBlock() {
+    this.setState({ blockNo: this.state.blockNo + 1 });
+    return this.drawBlockInBoard(this.state.blockNo);
   }
 
-  getNewBlock() {
-    return [2, 3, 12, 13];
+  getNextPosition() {
+    if (this.state.position[0] + 1 === this.state.board.length) {
+      return this.state.position;
+    }
+    return [this.state.position[0] + 1, this.state.position[1]];
+  }
+
+  getPreviousPosition() {
+    if (this.state.position[0] - 1 < 0) {
+      return this.state.position;
+    }
+    return [this.state.position[0] - 1, this.state.position[1]];
+  }
+
+  getRepeatedRows(arr, repeats) {
+    var func = (arr, repeats) =>[...Array.from({ length: repeats }, () => arr)];
+    return func(arr, repeats);
+  }
+
+  clearFilledRow(){
+    const board = this.state.board;
+    var filtered = board.filter((row) => !row.every(cell => cell > 1))
+    var newRows = this.getRepeatedRows(new Array(10).fill(0), board.length - filtered.length);
+    filtered =  newRows.concat(filtered)
+    this.setState({board: filtered});
+  }
+
+  drawMovingBlock() {
+    return this.drawBlockInBoard(1);
+  }
+
+  drawBlockInBoard(value) {
+    const board = this.state.board;
+    const block = this.state.block.content;
+
+    const len = block.length;
+    const x = this.state.position[0];
+    const y = this.state.position[1];
+
+    for (let i = len - 1; i >= 0; i--) {
+      for (let j = 0; j < len; j++) {
+        //Clear old block
+        if (i + x - 1 >= 0 && board[i + x - 1][j + y] === 1) {
+          board[i + x - 1][j + y] = 0;
+        }
+        //Draw new block
+        if (i + x >= 0 && i + x < board.length && block[i][j] === 1) {
+          board[i + x][j + y] = value;
+        }
+      }
+    }
+
+    return board;
+  }
+
+  hitNotMovingDot() {
+    const board = this.state.board;
+    const block = this.state.block.content;
+    const len = block.length;
+    const x = this.state.position[0];
+    const y = this.state.position[1];
+    for (let i = len - 1; i >= 0; i--) {
+        for (let j = 0; j < len; j++) {
+          
+          if (i + x >= 0 && i + x < board.length && block[i][j] === 1 && board[i + x][j + y] > 1 ) {
+            return true;
+          }
+        }
+      }
+    
+    return false;
+  }
+
+  moveBlock() {
+    if (this.isGameOver()) {
+      clearInterval(this.state.intervalId);
+      return;
+    }
+
+    let newBoard = this.state.block;
+    const isHitTheGround =
+      this.state.position[0] + this.state.block.content.length >
+      this.state.board.length;
+     
+    if (isHitTheGround || this.hitNotMovingDot()) {
+      this.setState({ position: this.getPreviousPosition() });
+      newBoard = this.pinCurrentBlock();
+      this.setState({ board: newBoard });
+      this.createNewBlock();
+      this.clearFilledRow();
+    } else {
+      newBoard = this.drawMovingBlock();
+      this.setState({ board: newBoard, position: this.getNextPosition() });
+    }
   }
 
   render() {
-    const board = this.state.dots.map((row, rowIdx) => {
+    const drawBoard = this.state.board.map((row, rowIdx) => {
       return (
         <div key={rowIdx}>
           {row.map((value, colIdx) => {
-            let isActivated =
-              this.state.movingBlock != null &&
-              this.state.movingBlock.includes(rowIdx * 10 + colIdx)
-                ? value === 0
-                : 0;
-            return <Dot key={colIdx} isActivated={isActivated} />;
+            return <Dot key={colIdx} isActivated={value !== 0} value={value} />;
           })}
         </div>
       );
     });
 
-    const status = this.state.gameOver ? "Game Over" : "Running";
     return (
-      <div>
-        <div>{status}</div>
-        <div>{board}</div>
-      </div>
+      <>
+        <div className="section-title">Next</div>
+
+        {drawBoard}
+      </>
     );
   }
 }
--- src/Tetris/Board2.js
+++ src/Tetris/Board2.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Dot from "./Commons/Dot";
-import Block from "./Commons/Block";
-import utils from "../Utils/utils";
 
 class Board2 extends React.Component {
   constructor(props) {
@@ -10,172 +8,93 @@ class Board2 extends React.Component {
     var dots = new Array(20);
     for (var i = 0; i < dots.length; i++) {
       dots[i] = new Array(10).fill(0);
-      // if(i === 19){
-      //   dots[i] = [2,2,2,2,0,0,2,2,2,2]
-      // }
     }
 
     this.state = {
-      board: dots,
-      block: this.generateMatrixWithBlock(Block.simpleRandom()),
-      position: [-3, 4],
-      blockNo: 1,
-      intervalId: null
+      dots: dots,
+      movingBlock: null,
+      gameOver: false      
     };
   }
 
   componentDidMount() {
-    const intervalId = setInterval(() => {
+    setInterval(() => {
       this.moveBlock();
     }, 500);
-    this.setState({intervalId: intervalId});
-  }
-
-  generateMatrixWithBlock(block) {
-    const len = Math.max(block.content.length, block.content[0].length);
-
-    block.content = utils.blockToMatrix(block.content, len, len);
-
-    return block;
-  }
-
-  createNewBlock() {
-    this.setState({
-      position: [-3, 4],
-      block: this.generateMatrixWithBlock(Block.tgm3RandomNew()),
-    });
-  }
-
-  isGameOver() {
-    const board = this.state.board;
-    const len = board[0].length;
-    for (let i = 0; i < len; i++) {
-      if (board[0][i] > 1) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  pinCurrentBlock() {
-    this.setState({ blockNo: this.state.blockNo + 1 });
-    return this.drawBlockInBoard(this.state.blockNo);
-  }
-
-  getNextPosition() {
-    if (this.state.position[0] + 1 === this.state.board.length) {
-      return this.state.position;
-    }
-    return [this.state.position[0] + 1, this.state.position[1]];
-  }
-
-  getPreviousPosition() {
-    if (this.state.position[0] - 1 < 0) {
-      return this.state.position;
-    }
-    return [this.state.position[0] - 1, this.state.position[1]];
-  }
-
-  getRepeatedRows(arr, repeats) {
-    var func = (arr, repeats) =>[...Array.from({ length: repeats }, () => arr)];
-    return func(arr, repeats);
-  }
-
-  clearFilledRow(){
-    const board = this.state.board;
-    var filtered = board.filter((row) => !row.every(cell => cell > 1))
-    var newRows = this.getRepeatedRows(new Array(10).fill(0), board.length - filtered.length);
-    filtered =  newRows.concat(filtered)
-    this.setState({board: filtered});
-  }
-
-  drawMovingBlock() {
-    return this.drawBlockInBoard(1);
-  }
-
-  drawBlockInBoard(value) {
-    const board = this.state.board;
-    const block = this.state.block.content;
-
-    const len = block.length;
-    const x = this.state.position[0];
-    const y = this.state.position[1];
-
-    for (let i = len - 1; i >= 0; i--) {
-      for (let j = 0; j < len; j++) {
-        //Clear old block
-        if (i + x - 1 >= 0 && board[i + x - 1][j + y] === 1) {
-          board[i + x - 1][j + y] = 0;
-        }
-        //Draw new block
-        if (i + x >= 0 && i + x < board.length && block[i][j] === 1) {
-          board[i + x][j + y] = value;
-        }
-      }
-    }
-
-    return board;
-  }
-
-  hitNotMovingDot() {
-    const board = this.state.board;
-    const block = this.state.block.content;
-    const len = block.length;
-    const x = this.state.position[0];
-    const y = this.state.position[1];
-    for (let i = len - 1; i >= 0; i--) {
-        for (let j = 0; j < len; j++) {
-          
-          if (i + x >= 0 && i + x < board.length && block[i][j] === 1 && board[i + x][j + y] > 1 ) {
-            return true;
-          }
-        }
-      }
-    
-    return false;
   }
 
   moveBlock() {
-    if (this.isGameOver()) {
-      clearInterval(this.state.intervalId);
+    if (this.state.gameOver) {
       return;
     }
 
-    let newBoard = this.state.block;
-    const isHitTheGround =
-      this.state.position[0] + this.state.block.content.length >
-      this.state.board.length;
-     
-    if (isHitTheGround || this.hitNotMovingDot()) {
-      this.setState({ position: this.getPreviousPosition() });
-      newBoard = this.pinCurrentBlock();
-      this.setState({ board: newBoard });
-      this.createNewBlock();
-      this.clearFilledRow();
-    } else {
-      newBoard = this.drawMovingBlock();
-      this.setState({ board: newBoard, position: this.getNextPosition() });
+    if (this.state.movingBlock == null) {
+      this.setState({
+        movingBlock: this.getNewBlock(),
+      });
+      return;
     }
+
+    let movingBlock = this.state.movingBlock.map((x) => {
+      x += 10;
+      return x;
+    });
+
+    let dots = this.state.dots;
+    let gameOver = this.state.gameOver;
+    if (this.shouldStop(movingBlock)) {
+      this.state.movingBlock.forEach(
+        (x) => (dots[Math.floor(x / 10)][x % 10] = 1)
+      );
+      const newBlock = this.getNewBlock();
+
+      if (this.shouldStop(newBlock)) {
+        gameOver = true;
+      } else {
+        movingBlock = newBlock;
+      }
+    }
+
+    this.setState({
+      dots: dots,
+      movingBlock: movingBlock,
+      gameOver: gameOver,
+    });
+  }
+
+  shouldStop(movingBlock) {
+    return (
+      movingBlock.some((x) => x >= 200) ||
+      movingBlock.some((x) => this.state.dots[Math.floor(x / 10)][x % 10] === 1)
+    );
+  }
+
+  getNewBlock() {
+    return [2, 3, 12, 13];
   }
 
   render() {
-    const drawBoard = this.state.board.map((row, rowIdx) => {
+    const board = this.state.dots.map((row, rowIdx) => {
       return (
         <div key={rowIdx}>
           {row.map((value, colIdx) => {
-            return <Dot key={colIdx} isActivated={value !== 0} value={value} />;
+            let isActivated =
+              this.state.movingBlock != null &&
+              this.state.movingBlock.includes(rowIdx * 10 + colIdx)
+                ? value === 0
+                : 0;
+            return <Dot key={colIdx} isActivated={isActivated} />;
           })}
         </div>
       );
     });
 
+    const status = this.state.gameOver ? "Game Over" : "Running";
     return (
-      <>
-        <div className="section-title">Next</div>
-
-        {drawBoard}
-      </>
+      <div>
+        <div>{status}</div>
+        <div>{board}</div>
+      </div>
     );
   }
 }
