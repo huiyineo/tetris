f7c003c80ecb784b5613995a567420b5555c4df1
huiyineo <huiyi2109@gmail.com>
2021-02-05 17:11:30 +0800

    Updates

---
+++ precache-manifest.080de1e90d72c39d5933d14d19af4897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd8e1fbc1de74ef16c11e9c824a680b",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "152f2fb6ac97b275ba0b",
+    "url": "/tetris/static/css/main.7bf1c939.chunk.css"
+  },
+  {
+    "revision": "52120af0cb63c493dd49",
+    "url": "/tetris/static/js/2.9f1f1c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tetris/static/js/2.9f1f1c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152f2fb6ac97b275ba0b",
+    "url": "/tetris/static/js/main.af2826dc.chunk.js"
+  },
+  {
+    "revision": "c179afea1aa9726cb16b",
+    "url": "/tetris/static/js/runtime-main.0d8747ab.js"
+  }
+]);
